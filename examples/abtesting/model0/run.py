b6e44da5b793cc3064397efc97ab9e6da94230bd
Simone Basso <bassosimone@gmail.com>
2025-02-26 17:26:51 +0100

    feat: start sketching out A/B testing with dt-model

---
+++ examples/abtesting/model0/run.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+
+def run_with_module(module):
+    fake_ensemble = [
+        (1, {"weekday": "monday"}),
+    ]
+
+    grid = {
+        module.drink_customers: np.linspace(0, 100, 10),
+        module.food_customers: np.linspace(0, 100, 10),
+    }
+
+    print("Now we can evaluate our model")
+    print(module.model.evaluate(grid, fake_ensemble))
+
+
+def run_with_dtmodel():
+    import model_dtm
+
+    run_with_module(model_dtm)
+
+
+def run_with_yakof():
+    import model_yakof
+
+    run_with_module(model_yakof)
+
+
+if __name__ == "__main__":
+    run_with_dtmodel()
+    run_with_yakof()
