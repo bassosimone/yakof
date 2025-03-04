ef48708949c892fb3d03b14a3942fd6cd3eba05c
Simone Basso <bassosimone@gmail.com>
2025-03-04 17:38:27 +0100

    cleanup: remove intermediate impelmentation

--- yakof/dtmodel/ensemble.py
+++
@@ -1,9 +0,0 @@
-from typing import Iterator as TypingIterator
-
-from . import context
-
-Weight = float
-
-VariableValue = dict[context.Variable, float]
-
-Iterator = TypingIterator[tuple[Weight, VariableValue]]
--- yakof/dtmodel/geometry.py
+++
@@ -1,5 +0,0 @@
-from ..frontend import bases, abstract, morphisms, spaces
-
-ComputationSpace = abstract.TensorSpace(bases.XYZ())
-
-ComputationTensor = abstract.Tensor[bases.XYZ]
