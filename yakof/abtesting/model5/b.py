236312e155703c18105f698da453bf9c114b42e8
Simone Basso <bassosimone@gmail.com>
2025-03-11 14:32:09 +0100

    feat(abtesting): start A/B testing with dt_model
    Code adapted from @maryamsajedi code in https://github.com/maryamsajedi/coffee_dt.
    
    Here's the diff between the two models:
    
    ```diff
    diff --git a/yakof/abtesting/model5/a.py b/yakof/abtesting/model5/b.py
    index c2feda8..5f881d6 100644
    --- a/yakof/abtesting/model5/a.py
    +++ b/yakof/abtesting/model5/b.py
    @@ -1,18 +1,16 @@
     """
    -A in the A/B testing
    +B in the A/B testing
     ====================
    
    -This module contains the original implementation of model5, with minor
    -changes required to adapt it for A/B testing.
    -
    -See https://github.com/maryamsajedi/coffee_dt/blob/master/coffee_dt_5.py.
    +This module contains the yakof-based implementation of model5, with minor
    +changes from the original, required to use yakof.
     """
    
    -from dt_model import (
    +from dt_model import Ensemble
    +from yakof.dtlang import (
         UniformCategoricalContextVariable,
         PresenceVariable,
         Constraint,
    -    Ensemble,
         Model,
         Index,
     )
    @@ -35,7 +33,7 @@ def run() -> np.ndarray:
             "saturday",
             "sunday",
         ]
    -    CV_weekday = UniformCategoricalContextVariable("weekday", [Symbol(v) for v in days])
    +    CV_weekday = UniformCategoricalContextVariable("weekday", days)
    
         # === Presence Variables ===
         drink_customers = PresenceVariable(
    @@ -92,7 +90,7 @@ def run() -> np.ndarray:
         )
    
         # === Evaluation ===
    -    ensemble = Ensemble(model, {CV_weekday: days}, cv_ensemble_size=7)
    +    ensemble = Ensemble(model, {CV_weekday: days}, cv_ensemble_size=7)  # type: ignore
    
         grid = {
             drink_customers: np.linspace(0, 100, 11),
    ```

---
+++ yakof/abtesting/model5/b.py
@@ -0,0 +1,104 @@
+"""
+B in the A/B testing
+====================
+
+This module contains the yakof-based implementation of model5, with minor
+changes from the original, required to use yakof.
+"""
+
+from dt_model import Ensemble
+from yakof.dtlang import (
+    UniformCategoricalContextVariable,
+    PresenceVariable,
+    Constraint,
+    Model,
+    Index,
+)
+from scipy.stats import triang
+
+import numpy as np
+from sympy import Symbol
+
+
+def run() -> np.ndarray:
+    """Runs the model and returns its result."""
+
+    # === Context Variables ===
+    days = [
+        "monday",
+        "tuesday",
+        "wednesday",
+        "thursday",
+        "friday",
+        "saturday",
+        "sunday",
+    ]
+    CV_weekday = UniformCategoricalContextVariable("weekday", days)
+
+    # === Presence Variables ===
+    drink_customers = PresenceVariable(
+        name="drink_customers",
+        cvs=[],
+    )
+    food_customers = PresenceVariable(
+        "food_customers",
+        cvs=[],
+    )
+
+    seat_capacity = Index("seat_capacity", 50)
+    # Most common service capacity is 100, ranging from 60 to 100
+    service_capacity = Index("service_capacity", triang(loc=80.0, scale=40.0, c=0.5))
+
+    # Usage of drink customers from the seats available in the bar
+    U_drink_seat = Index("drink customers seat usage factor", 0.2)
+
+    # Usage of food customers from the seats available in the bar
+    U_food_seat = Index("food customers seat usage factor", 0.8)
+
+    # Usage of drink customers from the service
+    U_service_drink = Index("drink customers service usage factor", 0.4)
+
+    # Usage of food customers from the service
+    U_service_food = Index("food customers service usage factor", 0.9)
+
+    # === Constraints ===
+    seat_constraint = Constraint(
+        usage=drink_customers * U_drink_seat + food_customers * U_food_seat,  # type: ignore
+        capacity=seat_capacity,
+    )
+
+    service_constraint = Constraint(
+        usage=food_customers * U_service_food + drink_customers * U_service_drink,  # type: ignore
+        capacity=service_capacity,
+    )
+
+    # === Model ===
+    model = Model(
+        "coffee_shop",
+        cvs=[CV_weekday],
+        pvs=[drink_customers, food_customers],
+        indexes=[
+            seat_capacity,
+            U_drink_seat,
+            U_food_seat,
+            U_service_drink,
+            U_service_food,
+            service_capacity,
+        ],
+        constraints=[seat_constraint, service_constraint],
+        capacities=[seat_capacity, service_capacity],
+    )
+
+    # === Evaluation ===
+    ensemble = Ensemble(model, {CV_weekday: days}, cv_ensemble_size=7)  # type: ignore
+
+    grid = {
+        drink_customers: np.linspace(0, 100, 11),
+        food_customers: np.linspace(0, 100, 11),
+    }
+
+    return model.evaluate(grid, ensemble)
+
+
+if __name__ == "__main__":
+    print(run())
