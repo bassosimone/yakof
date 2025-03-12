96e7fb96878fbdf62d19e4dd298332fd6e9ecbf1
Simone Basso <bassosimone@gmail.com>
2025-03-12 20:58:12 +0100

    x

--- yakof/sympyke/__init__.py
+++ yakof/sympyke/__init__.py
@@ -2,9 +2,8 @@
 ...
 """
 
-from .lambdify import lambdify
 from .operators import Eq
 from .piecewise import Piecewise
 from .symbol import Symbol
 
-__all__ = ["lambdify", "Eq", "Piecewise", "Symbol"]
+__all__ = ["Eq", "Piecewise", "Symbol"]
