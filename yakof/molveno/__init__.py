9398aca13401745c0278dba89e22c0900c2c500f
Simone Basso <bassosimone@gmail.com>
2025-03-13 08:21:11 +0100

    feat(molveno): add the original Molveno model
    This commit adds the original Molveno model to the repository.
    
    I have pulled this code from: https://github.com/fbk-most/dt-model/commit/efded93eee8956508faa265b3c5e071a48d11b23
    
    The changes compared to the original code has been adding
    docstrings, SPDX license identifiers, other minor comments,
    and reformatting using black.
    
    Moving forward, the general idea is to have here two versions
    of the model, one using yakof and the other using the original
    model, and to use them side by side.

---
+++ yakof/molveno/__init__.py
@@ -0,0 +1,12 @@
+"""
+Molveno Model
+=============
+
+This package contains the baseline Molveno model, in two
+distinct implementations, each in its own module:
+
+- `orig`: is the original model using dt_model
+- `yakof`: is the same model, but implemented using yakof
+"""
+
+# SPDX-License-Identifier: Apache-2.0
